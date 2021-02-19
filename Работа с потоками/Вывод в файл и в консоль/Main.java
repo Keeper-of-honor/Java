2e9798c103aadfa525a2951cede111e6ee6a7251
Keeper-of-honor <64023766+Keeper-of-honor@users.noreply.github.com>
2021-02-19 15:51:13 +0300

    Update Main.java

--- Работа с потоками/Вывод в файл и в консоль/Main.java
+++ Работа с потоками/Вывод в файл и в консоль/Main.java
@@ -12,11 +12,11 @@ import java.io.PrintStream;
 public class Main
 {
 	public static void main(String[] args) throws Exception{
-	    PrintStream console = new PrintStream(System.out);  //Запоминаем поток вывода в консоль
-	    
-	    //Меняем поток на вывод в файл "file.txt"
-	    FileOutputStream f = new FileOutputStream("file.txt");
-	    System.setOut(new PrintStream(f));
+		PrintStream console = new PrintStream(System.out);  //Запоминаем поток вывода в консоль
+		
+		//Меняем поток на вывод в файл "file.txt"
+		FileOutputStream f = new FileOutputStream("file.txt");
+		System.setOut(new PrintStream(f));
 		System.out.println("File");
 		
 		//Возвращаем старый поток (вывод в консоль)
